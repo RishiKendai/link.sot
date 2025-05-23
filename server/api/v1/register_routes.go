927adffb64f84350664d1fe5964e7c99a8ead75a
Rishikesh <kendairishikesh5@gmail.com>
2025-05-23 09:11:24 +0530

    feat: add router handler

---
+++ server/api/v1/register_routes.go
@@ -0,0 +1,13 @@
+package apiV1
+
+import (
+	"github.com/RishiKendai/sot/api/v1/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.RouterGroup) {
+	routes.Register(router)
+	routes.Login(router)
+	routes.Links(router)
+	routes.Analytics(router)
+}
