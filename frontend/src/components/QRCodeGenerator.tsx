de13b2446f7192aad7c57f5037e9a5b190ac39de
Rishikesh <kendairishikesh5@gmail.com>
2025-08-04 14:14:26 +0530

    style: fix qr code style

---
+++ frontend/src/components/QRCodeGenerator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import QRCode from "react-qr-code";
+
+interface QRCodeGeneratorProps {
+    url: string;
+    size?: number;
+    className?: string;
+    isQR?: boolean; // Whether to generate QR format URL
+}
+
+const QRCodeGenerator: React.FC<QRCodeGeneratorProps> = ({
+    url,
+    size = 32,
+    className = '',
+}) => {
+
+    return (
+        <div className={`${className}`}>
+            <QRCode
+                value={url}
+                size={size}
+                style={{ height: 'auto', maxHeight: size, maxWidth: '100%', width: '100%' }}
+                viewBox='0 0 256 256'
+            />
+        </div>
+    );
+};
+
+export default QRCodeGenerator; 
