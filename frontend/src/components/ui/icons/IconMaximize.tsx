de72dd017af34f753714d810d2f05ef53a274f68
Rishikesh <kendairishikesh5@gmail.com>
2025-08-04 15:19:07 +0530

    feat: add icons

---
+++ frontend/src/components/ui/icons/IconMaximize.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IconBase } from './IconBase';
+import type { IconBaseProps } from './IconBase';
+
+const IconMaximize: React.FC<IconBaseProps> = (props) => (
+    <IconBase {...props}>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 8v-2a2 2 0 0 1 2 -2h2" />
+        <path d="M4 16v2a2 2 0 0 0 2 2h2" />
+        <path d="M16 4h2a2 2 0 0 1 2 2v2" />
+        <path d="M16 20h2a2 2 0 0 0 2 -2v-2" />
+    </IconBase>
+)
+
+
+export default IconMaximize;
---
+++ frontend/src/components/ui/icons/IconMinimize.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IconBase } from './IconBase';
+import type { IconBaseProps } from './IconBase';
+
+const IconMinimize: React.FC<IconBaseProps> = (props) => (
+    <IconBase {...props}>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M15 19v-2a2 2 0 0 1 2 -2h2" />
+        <path d="M15 5v2a2 2 0 0 0 2 2h2" />
+        <path d="M5 15h2a2 2 0 0 1 2 2v2" />
+        <path d="M5 9h2a2 2 0 0 0 2 -2v-2" />
+    </IconBase>
+)
+
+
+export default IconMinimize;
