c80363dcaac3d477890b16c29f18687fb1c5fd71
Rishikesh <kendairishikesh5@gmail.com>
2025-08-10 11:56:15 +0530

    fix: add loader

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import { Toaster } from 'sonner';
 import clsx from 'clsx';
+import Loader from './components/ui/Loader';
 
 export default function NavWrapper() {
   const location = useLocation();
@@ -10,7 +11,7 @@ export default function NavWrapper() {
 
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<FallbackLoader />}>
         {isLandingPage ? '' : <Sidebar />}
         <main className={clsx('flex', isLandingPage ? 'pt-0' : 'pt-24 md:pt-0')}>
           <Outlet />
@@ -20,3 +21,9 @@ export default function NavWrapper() {
     </>
   );
 }
+
+const FallbackLoader: React.FC = () => (
+  <div className='h-screen flex items-center justify-center'>
+    <Loader color='#6366f1' className='w-12 h-12'  />
+  </div>
+);
