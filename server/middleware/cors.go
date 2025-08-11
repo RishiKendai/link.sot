c59ec2b8af19d212189b567d78ec13c111b1d79a
Rishikesh <kendairishikesh5@gmail.com>
2025-08-12 01:28:26 +0530

    fix: cors

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -1,30 +1,26 @@
 package middleware
 
 import (
-	"slices"
-	"strings"
-
-	"github.com/RishiKendai/sot/pkg/config/env"
 	"github.com/gin-gonic/gin"
 )
 
-var allowedOrigins = strings.Split(env.GetEnvKey("CORS_DOMAINS"), ",")
+// var allowedOrigins = strings.Split(env.GetEnvKey("CORS_DOMAINS"), ",")
 
-func isAllowedOrigin(origin string) bool {
-	return slices.Contains(allowedOrigins, origin)
-}
+// func isAllowedOrigin(origin string) bool {
+// 	return slices.Contains(allowedOrigins, origin)
+// }
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
+		// origin := c.Request.Header.Get("Origin")
 
-		if isAllowedOrigin(origin) {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		} else {
-			// Fallback to localhost for development
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-		}
-
+		// if isAllowedOrigin(origin) {
+		// 	c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		// } else {
+		// 	// Fallback to localhost for development
+		// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		// }
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://app.linksot.space/")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, Location")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
